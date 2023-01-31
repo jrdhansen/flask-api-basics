fd5c1d833e2fc2da0565b504dc66266f74cdb379
jrdhansen <jrdhansen@gmail.com>
2023-01-30 21:00:48 -0700

    Update file with JSON object, returned with /bye route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask     # Import flask package and the Flask class
+from flask import Flask, jsonify     # Import flask package and the Flask class
 app = Flask(__name__)       # Create a Flask class instance. The first arg is the name of the app's module or package.
                             # __name__ is just a convention
 
@@ -14,9 +14,24 @@ def hi_there_everyone():
 @app.route('/bye')
 def bye():
     # Prepare a response for the request that came to /bye
-    c = (2*500)/0
-    s = str(c)
-    return 'bye_'+s
+    #c = (2*500)/0
+    #s = str(c)
+    my_age = 29_000
+    ret_json = {
+        'name': 'jhansen',
+        'age': my_age,
+        'phones': [
+            {
+                'phone_name': 'Pixel_6A',
+                'phone_number': '435-812-9485'
+            },
+            {
+                'phone_name': 'iPhone_14',
+                'phone_number': '801-274-0031'
+            }
+        ]
+    }
+    return jsonify(ret_json)
 
 if __name__ == '__main__':
     app.run()  # leave this empty until specifying an IP and port. If debug=True in run() method you will get error 
