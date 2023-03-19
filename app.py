13ea6fc0d5a5f48673a342f6bf362d602ccf19f4
jrdhansen <jrdhansen@gmail.com>
2023-03-18 19:52:20 -0600

    Add initial post route to app

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify     # Import flask package and the Flask class
+from flask import request   # has all of the info about POSTs
 app = Flask(__name__)       # Create a Flask class instance. The first arg is the name of the app's module or package.
                             # __name__ is just a convention
 
@@ -32,6 +33,17 @@ def bye():
         ]
     }
     return jsonify(ret_json)
+
+@app.route('/add_two_nums', methods=["POST"])
+def add_two_nums():
+    pass
+    # Get x,y from the POSTed data
+
+    # Add z = x+y
+
+    # Prepare a JSON "z":z
+
+    # return jsonify(map_prepared)
 
 if __name__ == '__main__':
     app.run()  # leave this empty until specifying an IP and port. If debug=True in run() method you will get error 
